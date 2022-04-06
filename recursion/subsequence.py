3fa0595343e2a81a56212e3b5c0967de6388f4d1
Vince Tran <vince.tran@cabelas.ca>
2022-04-06 17:00:54 -0500

    add increasing subseq recursive

--- recursion/subsequence.py
+++ recursion/subsequence.py
@@ -25,6 +25,24 @@ def get_all_subsequences(A: list):
     return subseqs
 
 
+def get_all_increasing_subsequences_recursive(A: list):
+    '''Recursive method
+    '''
+    if len(A) <= 1:
+        return [[], A]
+    elif len(A) == 2:
+        if A[0] < A[1]:
+            return [[], [A[0]], [A[1]], A]
+        else:
+            return [[], [A[1]], [A[0]]]
+    else:
+        subseqs = get_all_increasing_subsequences_recursive(A[1:])
+        subseqs = subseqs \
+            + [ [A[0]] ] \
+            + [ [A[0]] + subseq for subseq in subseqs if subseq and A[0] < subseq[0] ]
+        return subseqs
+
+
 def get_all_increasing_subsequences(A: list):
     '''Iterative method
     '''
@@ -40,10 +58,12 @@ def get_all_increasing_subsequences(A: list):
 
 
 if __name__ == "__main__":
-    test_list = [3,5,2,4]
+    test_list = [5,5]
     subseqs_recur = get_all_subsequences_recursive(test_list)
     print(f"Subsequences using recursive: {subseqs_recur}")
-    subseqs = get_all_subsequences(test_list)
-    print(f"Subsequences using iterative: {subseqs}")
-    subseqs = get_all_increasing_subsequences(test_list)
-    print(f"Increasing subsequences using iterative: {subseqs}")
+    subseqs_iter = get_all_subsequences(test_list)
+    print(f"Subsequences using iterative: {subseqs_iter}")
+    incr_subseqs_recur = get_all_increasing_subsequences_recursive(test_list)
+    print(f"Increasing subsequences using recursive: {incr_subseqs_recur}")
+    incr_subseqs_iter = get_all_increasing_subsequences(test_list)
+    print(f"Increasing subsequences using iterative: {incr_subseqs_iter}")
