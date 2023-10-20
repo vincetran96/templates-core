7dc121d3dd4c05dde6325bead6dec4d7895fa0ed
vincetran96 <vietanhtran.96@gmail.com>
2023-10-20 15:52:35 +0700

    Add another method for reversing str

--- programming_patterns/recursion/reverse_str.py
+++ programming_patterns/recursion/reverse_str.py
@@ -2,3 +2,14 @@ def reverse(s: str):
     if len(s) == 1:
         return s
     return s[len(s) - 1] + reverse(s[:len(s) - 1])
+
+
+def reverse_1(s: str):
+    if len(s) == 1:
+        return s
+    return reverse(s[1:]) + s[0]
+
+
+if __name__ == "__main__":
+    print(reverse("abc"))
+    print(reverse_1("abc"))
