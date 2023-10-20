7cb7d4f6436d137cba922c9ca614ef6f09d37f87
Vince Tran <vietanhtran.96@gmail.com>
2023-10-21 00:02:54 +0700

    Add du linux command

--- linux_commands/io.sh
+++ linux_commands/io.sh
@@ -2,3 +2,6 @@
 
 # Delete all __pycache__ dirs recursively in current dir
 find . -type d -name "__pycache__" -exec rm -r {} +
+
+# Show disk usage of things at the top level in current dir
+du -hd1
