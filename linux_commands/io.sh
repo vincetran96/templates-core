0d180433cffcdf6e086d18e16a9d5ad3343423e8
Vince Tran <vietanhtran.96@gmail.com>
2023-10-21 14:24:54 +0700

    Add network commands

--- linux_commands/io.sh
+++ linux_commands/io.sh
@@ -3,5 +3,5 @@
 # Delete all __pycache__ dirs recursively in current dir
 find . -type d -name "__pycache__" -exec rm -r {} +
 
-# Show disk usage of things at the top level in current dir
-du -hd1
+# Show disk usage of dirs (?) at the top level in current dir
+du -hd1 | sort -h
---
+++ linux_commands/network.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+# Check what things are listening
+lsof -i -P -n | grep LISTEN
+
+# Check my DNS settings
+cat /etc/resolv.conf
