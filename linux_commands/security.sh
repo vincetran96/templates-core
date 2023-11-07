d5fcefad940a00637f90eea27b619d622ceaf2ca
Vince Tran <vietanhtran.96@gmail.com>
2023-11-07 07:53:51 +0700

    Add SHA of text in Mac terminal

--- linux_commands/security.sh
+++ linux_commands/security.sh
@@ -1,2 +1,6 @@
 # SHA hash of text in Mac terminal
 echo -n zxcv | shasum -a 1
+
+# SHA hash of content of a text file in Mac terminal
+# Ignore all newlines
+awk '{printf "%s", $0} END {printf ""}' filename | shasum -a 1
