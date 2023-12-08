61d40a1dbd0e1e83965020ffa19669b984ad6c15
Vince Tran <vietanhtran.96@gmail.com>
2023-12-09 00:07:07 +0700

    Add sort for SinglyLinkedList

--- programming_patterns/data_structures/collections/linked_list.py
+++ programming_patterns/data_structures/collections/linked_list.py
@@ -7,17 +7,18 @@ class Node:
     """
     def __init__(self, value: int) -> None:
         self.value = value
-        self.next: Node = None # type: ignore
+        self.next: Node = None  # type: ignore
 
 
 class SinglyLinkedList:
     """Implementation of Singly Linked List
     """
     def __init__(self) -> None:
-        self.head: Node = None # type: ignore
-        self.tail: Node = None # type: ignore 
+        self.head: Node = None  # type: ignore
+        self.tail: Node = None  # type: ignore
 
     def is_empty(self) -> bool:
+        """Checks empty"""
         return self.head is None
 
     def add(self, value: int) -> None:
@@ -32,10 +33,29 @@ class SinglyLinkedList:
             self.tail = node
 
     def get_head(self) -> int:
+        """Gets head"""
         return self.head.value
 
     def get_tail(self) -> int:
+        """Gets tail"""
         return self.tail.value
+
+    def sort(self):
+        """Sorts itself"""
+        def _sort(base_node: Node, compare_node: Node):
+            """Sort helper"""
+            if base_node.next is not None:
+                if compare_node.value < base_node.value:
+                    temp_value = compare_node.value
+                    compare_node.value = base_node.value
+                    base_node.value = temp_value
+                if compare_node.next is not None:
+                    _sort(base_node, compare_node.next)
+                else:
+                    _sort(base_node.next, base_node.next.next)
+
+        if len(self) > 1:
+            _sort(self.head, self.head.next)
 
     def __repr__(self) -> str:
         def _disp(node: Node, final_str: str = "") -> str:
@@ -45,6 +65,14 @@ class SinglyLinkedList:
 
         return "[" + _disp(self.head).strip(", ") + "]"
 
+    def __len__(self) -> int:
+        def _getsize(node: Node, acc: int):
+            if node is None:
+                return acc
+            return _getsize(node.next, acc + 1)
+
+        return _getsize(self.head, 0)
+
 
 if __name__ == "__main__":
     n0 = Node(10)
@@ -52,15 +80,19 @@ if __name__ == "__main__":
 
     sl = SinglyLinkedList()
     print(sl.is_empty())
-    sl.add(1)
+    sl.add(50)
     print(f">> sl head={sl.get_head()}, sl tail={sl.get_tail()}")
     sl.add(2)
     print(f">> sl head={sl.get_head()}, sl tail={sl.get_tail()}")
     print(f"sl head.next={sl.head.next.value}")
     print("sl as string:")
     print(sl)
-    sl.add(3)
+    sl.add(65)
+    sl.add(12)
     print(f">> sl head={sl.get_head()}, sl tail={sl.get_tail()}")
     print(f"sl head.next={sl.head.next.value}")
     print("sl as string:")
+    print(sl)
+    print(len(sl))
+    sl.sort()
     print(sl)
