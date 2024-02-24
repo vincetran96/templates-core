ee7fe1db4386bc65161d6dce85834a31de99b950
Vince Tran <vietanhtran.96@gmail.com>
2024-02-24 15:07:29 +0700

    Add another solution for leetcode easy_859

--- programming_patterns/_leetcode/easy_859/easy_859.py
+++ programming_patterns/_leetcode/easy_859/easy_859.py
@@ -15,3 +15,16 @@ class Solution:
         if len(diff_idxs) == 0 and any(map(lambda x: x >= 2, same_dict.values())):
             return True
         return False
+
+
+class Solution1:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal) or len(s) == 1:
+            return False
+        diff_idxs = [ i for i in range(len(s)) if s[i] != goal[i] ]
+        if len(diff_idxs) == 2:
+            if s[diff_idxs[1]] + s[diff_idxs[0]] == goal[diff_idxs[0]] + goal[diff_idxs[1]]:
+                return True
+        if len(diff_idxs) == 0 and len(set(s)) < len(s):
+            return True
+        return False
