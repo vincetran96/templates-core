f1c15689aaf777083de95f411d15ae450ac9b9b0
anhtv <anhtv@coccoc.com>
2024-05-21 17:18:41 +0700

    Change from ifconfig to ip

--- homelab/work/connect.sh
+++ homelab/work/connect.sh
@@ -2,7 +2,7 @@
 cd /home/username/work/Company/staff-vpn-2025
 sudo openvpn --config staff_ca_2025.ovpn --auth-user-pass /home/username/work/Company/ldap &
 while true; do
-    if ifconfig "tun0" >/dev/null 2>&1; then
+    if ip a show "tun0" >/dev/null 2>&1; then
         echo "Network interface tun0 is available."
         break
     else
--- homelab/work/disconnect.sh
+++ homelab/work/disconnect.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-sudo ifconfig tun0 down
+sudo ip link delete tun0
 sudo killall -SIGINT openvpn
-# sudo ip link delete tun0
 echo "Done."
